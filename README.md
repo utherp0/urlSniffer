0b12701e94b8d8eee0c8cc9974886a0ffcb7546b
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2017-07-20 13:28:26 +0100

    First drop

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # urlSniffer
 Analytic sniffer for gauging the originating server from an obfuscated URL
+
+Simple application for sniffing URLs to detect Server of source.
---
+++ src/main/java/org/uth/urlSniffer/utils/URLSniffer.java
@@ -0,0 +1,78 @@
+package org.uth.urlSniffer.utils;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * URLSniffer - takes a list of target URLs, establishes a connections, polls the
+ * http response headers to see source Server, returns count of provided target server
+ * @author Ian 'Uther' Lawson
+ */
+public class URLSniffer 
+{
+  public static void main( String[] args )
+  {
+    if( args.length != 2 )
+    {
+      System.out.println( "Usage java URLSniffer (comma seperated target URLs) (comma separated sub-search server target)");
+      System.exit(0);
+    }
+    
+    new URLSniffer( args[0], args[1] );
+  }
+  
+  public URLSniffer( String urlWhiteList, String serverSubWhiteList )
+  {
+    URLSniffer.log( "Starting...");
+    
+    // Extract the list of target URLs
+    String[] targetURLs = urlWhiteList.split(",");
+    
+    // Extract the substring match for target server determination
+    String[] serverSubSearch = serverSubWhiteList.split( "," );
+    
+    // Loop through the target URLs
+    for( String target : targetURLs )
+    {      
+      //URLSniffer.log( "Sniffing " + target );
+      
+      try
+      {
+        URL targetURL = new URL( target );
+        
+        HttpURLConnection connection = (HttpURLConnection)targetURL.openConnection();
+        
+        this.pollResponseHeader(connection, serverSubSearch);
+      }
+      catch( Exception exc )
+      {
+        exc.printStackTrace();
+        URLSniffer.log( "Invalid URL format for target " + target );
+      }
+    }
+    
+    URLSniffer.log( "Complete...");
+  }
+  
+  private void pollResponseHeader( HttpURLConnection target, String[] subSearch )
+  {
+    // Short circuit search
+    for( String subSearchComponent : subSearch )
+    {
+      String server = target.getHeaderField("Server");
+      
+      if( server != null )
+      {
+        if( server.indexOf(subSearchComponent) != -1 )
+        {
+          URLSniffer.log( "MATCH " + subSearchComponent + " " + target.getURL().toString() );
+        }
+      }
+    }
+  }
+  
+  private static void log( String message )
+  {
+    System.out.println( "[URLSniffer] " + message );
+  }
+}
